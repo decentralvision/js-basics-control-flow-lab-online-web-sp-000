d8d83bf8c0f5b9d5af48fc6e892fd08c88014eb4
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-05-30 00:53:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!'
   } else if ( feet > 2500) {
-    return 'I will gladly take your thirty bucks.'
-  } else if (feet > 2000) {
     return 'No can do.'
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
   }
 }
